effb02ed30ee99bcce7ea8c4c86755dfee8f45ca
Nate Wolfe <wolfe.nate26@gmail.com>
2022-07-22 15:26:40 -0600

    added seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,11 @@ VALUES ("Engineering"), ("Housekeeping"), ("Maintenance")
 INSERT INTO roleOf ()
 VALUES ("Manager", 67000.00 ,1),
 ("Peon", 50000.00 , 1)
+("Plebeon", 50000.00, 2)
+("Jason", 60000.00, 3)
 
 INSERT INTO employee ()-- employees here
 VALUES ("Nate", "Wolfe", 1, NULL),
-
+("Luke", "Renoe", 2),
+("John", "Jacobson", 3),
+("Ronald", "Weasley", 5);
